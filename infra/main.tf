4b53adb8a45aed58c602c0fea790326954c731db
Balazs Bereczky <balazs.bereczky@wecanconsulting.eu>
2022-09-26 12:25:31 +0200

    terrafrom deploy

--- infra/main.tf
+++ infra/main.tf
@@ -18,3 +18,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "CloudSummit-01"
   location = "ukw"
 }
+
+resource "azurerm_subscription" "storage" {
+  alias             = "storagesub"
+  subscription_name = "My Storage Subscription"
+  subscription_id   = "71818d77-4e18-47c8-8c36-b7af11e60bde"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = lower("bicep_moon_storage_111_${azurerm_subscription.storage.subscription_id}")
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
